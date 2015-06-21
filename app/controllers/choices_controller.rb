f64f008c09506720710553eee40159b408f14bc9
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-21 18:52:17 -0400

    updated choices controller for ajaxx

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,8 +1,11 @@
 class ChoicesController < ApplicationController
   def create
     if request.xhr?
+
+      @survey = Survey.find_by(id: params[:survey_id])
       @choice = Choice.create(text: params[:text], question_id: params[:question_id])
-      render partial: ""
+      render partial: "choices/choice_text", locals: {choice: @choice, survey: @survey}
+    end
     # @choice = Choice.create(choice_params)
     # redirect_to :back
   end

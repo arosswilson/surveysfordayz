fce78191ec95971c7b6de13722855d30a328587e
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-21 15:48:32 -0400

    adding a lot of ajax calls

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -15,3 +15,68 @@ $(document).ready(function(){
     });
   });
 });
+
+$(document).ready(function() {
+  $(".new_survey").on("submit", function(event) {
+    event.preventDefault();
+    var $title = $("#survey_title").val();
+    $.ajax({
+      url: "/surveys",
+      method: "POST",
+      data: {title: $title}
+    })
+    .done(function(newQuestionForm){
+      $("#new_survey").hide();
+      $(".questions").prepend(newQuestionForm);
+      $(document).ready(function() {
+        $("#new_question").on("submit", function(event) {
+          event.preventDefault();
+          var $question = $("#question_text").val();
+          $.ajax({
+            url: $("#new_question").attr("action"),
+            method: "POST",
+            data: {text: $question}
+          })
+          .done(function (newChoiceForm){
+            $("#question_text").val("");
+            $(".questions").append(newChoiceForm);
+            $(document).ready(function () {
+              $("#new_choice").on("submit", function(event) {
+                event.preventDefault();
+                var $choice_path = $("#new_choice").attr("action");
+                var $choice = $("#choice_text").val();
+                var $question_id = parseInt($("#choice_question_id").attr("value"))
+                $.ajax({
+                  url: $choice_path,
+                  method: "POST",
+                  data: {text: $choice,question_id: $question_id}
+                })
+                .done(function () {
+                  console.log("hell yeah!");
+                })
+                .fail(function () {
+                  console.log("sad little baby");
+                })
+              });
+            });
+          })
+          .fail(function () {
+            console.log("sad tiger");
+          })
+        });
+      });
+    })
+    .fail(function() {
+      console.log("sad panda");
+    });
+  });
+
+
+});
+
+$(document).ready(function() {
+  $("#new_question").on("submit", function(event) {
+    event.preventDefault();
+    debugger;
+  });
+});

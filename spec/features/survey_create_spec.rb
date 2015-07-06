9547c445a5af181cd853bd2f2729575e24a51a69
JMC11 & Taylor Hang <pair+JMC11.sixthand6th@devbootcamp.com>
2015-07-06 11:21:52 -0400

    refactor user auth with before each block

--- spec/features/survey_create_spec.rb
+++ spec/features/survey_create_spec.rb
@@ -3,21 +3,19 @@ require 'pry'
 
 feature 'User auth' do
   given!(:user) { User.create(email: "bob@bob.com", password: "pants") }
-
-  it 'should login a user' do
+  before(:each) do
     visit root_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Login'
+  end
+
+  it 'should login a user' do
     greeting = "Hello #{user.email}!"
     expect(page).to have_content greeting
   end
 
   it 'should logout a user' do
-    visit root_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Login'
     click_button 'Logout'
     expect(page).to have_link "Click to create account"
   end
@@ -31,6 +29,7 @@ feature 'Create survey' do
     fill_in 'Password', with: user.password
     click_button 'Login'
   end
+
   it 'should show create survey button' do
     expect(page).to have_button "Create a Survey"
   end

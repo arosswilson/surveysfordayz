d0b1a5791fe2c4dee4863f61b9064eb9e698a5b8
JMC11 & Taylor Hang <pair+JMC11.sixthand6th@devbootcamp.com>
2015-07-06 16:35:37 -0400

    create working post create test

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,1 +1,25 @@
 require 'rails_helper'
+
+
+describe SurveysController do
+  let!(:user) {User.create(:email => "bob@bob.com", :password => "something")}
+  let!(:survey) {Survey.create(:title => "tits mcgee", :user_id => user.id)}
+
+  describe "GET surveys/new" do
+    it "should display new survey page" do
+     get :new
+     expect(response).to be_ok
+     expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST /surveys" do
+    it "should create a new survey" do
+      session[:user_id] = user.id
+      post :create, survey: {title: survey.title, user_id: user.id}
+      expect(response).to be_redirect
+      # expect(response).to redirect_to(survey)
+    end
+  end
+
+end

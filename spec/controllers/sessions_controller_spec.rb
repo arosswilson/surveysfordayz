1e32f6fe20f1c0b91d9d4814ac78faa395ddc627
JMC11 & Taylor Hang <pair+JMC11.sixthand6th@devbootcamp.com>
2015-07-06 15:56:09 -0400

    add another test - complete for the day - we'll be out at the bar for drinks thank you very much

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,6 +5,8 @@ describe SessionsController do
   describe "GET /new" do
     it "should take to login page" do
      get :new
+     expect(response).to be_ok
+     expect(response).to render_template(:new)
     end
   end
 
